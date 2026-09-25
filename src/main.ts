accf46212c95555780238ebb156ba082f9b80867
Tomas Salina <salinatomass53@gmail.com>
2026-09-24 21:21:21 -0300

    chore: add global validation pipe, helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { StandardSchemaValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
+  app.useGlobalPipes(new StandardSchemaValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
